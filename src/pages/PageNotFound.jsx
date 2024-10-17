f2b9ea9c1f1b8e6aa9016996cfeb6dfdd439aee6
trngkhang <nguyenkhangdev@gmail.com>
2024-10-17 16:40:08 +0700

    edit pagenotfound

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,13 +1,21 @@
 export default function PageNotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-xl mb-8">Oops! Page Not Found</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img
+        src="https://cdn.prod.website-files.com/62fec8041edae129b05e72ac/63a162b657a05c31e92ebbd5_Blog-HeroImage-404-Page-Error-1600x900px.png"
+        alt="Page not found illustration"
+        className="max-w-72 mb-4"
+      />
+      <h1 className="text-3xl mb-8 text-center">
+        <span>Xin lỗi!</span>
+        <br />
+        <span>Chúng tôi không tìm thấy trang mà bạn cần!</span>
+      </h1>
       <a
         href="/"
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+        className="px-4 py-2 text-xl bg-teal-600 text-white rounded hover:bg-teal-700 transition"
       >
-        Go Back Home
+        Trở về trang chủ
       </a>
     </div>
   );
